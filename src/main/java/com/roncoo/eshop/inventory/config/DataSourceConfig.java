1ce1d0ca986a63e5fa87ebb2f0b8b2396bc2231e
上官永忠 <wb-sgyz272053@alibaba-inc.com>
2018-03-16 18:20:47 +0800

    修改config

--- src/main/java/com/roncoo/eshop/inventory/config/DataSourceConfig.java
+++ src/main/java/com/roncoo/eshop/inventory/config/DataSourceConfig.java
@@ -1,7 +1,50 @@
 package com.roncoo.eshop.inventory.config;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
 /**
  * 后面构建config
  */
+//@Configuration
+//@EnableJpaRepositories(
+//    basePackages = "com.i.commons.repository",
+//    entityManagerFactoryRef = "EntityManager",
+//    transactionManagerRef = "TransactionManager"
+//)
+
+@Configuration
 public class DataSourceConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix="spring.datasource")
+    public org.apache.tomcat.jdbc.pool.DataSource dataSource() {
+        return new org.apache.tomcat.jdbc.pool.DataSource();
+    }
+
+
+    @Bean
+    @Primary
+    public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource());
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/mybatis/*.xml"));
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    @Primary
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
 }
